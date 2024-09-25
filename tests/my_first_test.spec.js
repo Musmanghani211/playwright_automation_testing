4f7fdb4044334de05cc8446cd17d1f5e91b60c58
Musmanghani211 <musmanghani0211@gmail.com>
2024-09-25 14:34:13 +0500

    all change

--- tests/my_first_test.spec.js
+++ tests/my_first_test.spec.js
@@ -1,7 +1,12 @@
 const { test, expect } = require("@playwright/test");
-const { hello, helloword } = require("./demo/hello.js");
+// const { hello, helloword } = require("./demo/hello.js");
 
 // import { hello, helloword } from "./demo/hello.js";
 
-console.log(hello());
-console.log(helloword());
+// console.log(hello());
+// console.log(helloword());
+
+test("my first test", async ({ page }) => {
+  await page.goto("https://google.com");
+  await expect(page).toHaveTitle("Google");
+});
